dc9995e964ababd4fb525cf4f4ff313e96aee3dd
Alberto Lucas <38790522+AlbertoLucass@users.noreply.github.com>
2020-08-21 22:22:19 -0300

    Projeto Finalizado!!!

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { StylesProvider } from '@material-ui/styles'
-import { CssBaseline } from '@material-ui/core'
-import GlobalStyle from './commons/styles/global-style'
-import Main from './containers/Main'
+import { StylesProvider } from '@material-ui/styles';
+import { CssBaseline } from '@material-ui/core';
+import GlobalStyle from './commons/styles/global-style';
+import Main from './containers/Main';
 
 function App() {
   return (
-    <StylesProvider injectFirst>
+    <StylesProvider> 
       <CssBaseline />
       <GlobalStyle />
+
       <Main />
+
     </StylesProvider>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,18 +1,18 @@
-import { Card, CardContent } from './Card'
-import Button from './Button'
-import Grid from './Grid'
-import Typography from './Typography'
-import Skeleton from './Skeleton'
-import Select from './Select'
-import MenuItem from './MenuItem'
+import { Card, CardContent } from './Card';
+import Button from './Button';
+import Grid from './Grid';
+import Typography from './Typography';
+import Skeleton from './Skeleton';
+import Select from './Select';
+import MenuItem from './MenuItem';
 
 export {
-  Button,
-  Card,
-  CardContent,
-  Grid,
-  Typography,
-  Skeleton,
-  Select,
-  MenuItem
+    Button,
+    Card,
+    CardContent,
+    Grid,
+    Typography,
+    Skeleton,
+    Select,
+    MenuItem
 }
