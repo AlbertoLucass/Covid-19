8f8c4996e8bade016f28e45ecd8c5a8d117bd22d
Alberto Lucas <38790522+AlbertoLucass@users.noreply.github.com>
2020-08-21 22:26:46 -0300

    Projeto Finalizado!!!

--- src/containers/Main/components/Card.jsx
+++ src/containers/Main/components/Card.jsx
@@ -1,17 +1,21 @@
-import React, { memo } from 'react';
-import PropTypes from 'prop-types';
-import { Card as CardUI } from '../../../components';
-import { LabelStyled, ValueStyled, CardContentStyled } from './style';
+import React, { memo } from 'react'
+import PropTypes from 'prop-types'
+import { Card as CardUI } from '../../../components'
+import {
+  LabelStyled,
+  ValueStyled,
+  CardContentStyled
+} from './style'
 
-function Card({ value, label, color }){
-    return(
-        <CardUI>
-            <CardContentStyled color={color}>
-                <ValueStyled>{value}</ValueStyled>
-                <LabelStyled>{label}</LabelStyled>
-            </CardContentStyled>
-        </CardUI>
-    )
+function Card({ value, label, color }) {
+  return (
+    <CardUI>
+      <CardContentStyled color={color}>
+        <ValueStyled>{value}</ValueStyled>
+        <LabelStyled>{label}</LabelStyled>
+      </CardContentStyled>
+    </CardUI>
+  )
 }
 
-export default memo(Card);
+export default memo(Card)
